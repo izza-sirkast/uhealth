f9ad0b3419577de7368b1fb686c828b22956e49b
Dawla Izza <dawlaizza405@gmail.com>
2024-02-15 10:03:49 +0700

    Form surat izin sudah berjalan dan menyimpan data ke database

--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -19,6 +19,25 @@ router.get('/izin-sakit', (req, res) => {
     res.render('mahasiswa/izin-sakit')
 })
 
+router.post('/izin-sakit', async (req, res) => {
+    const {tanggal, penyakit, klinik, pemeriksa} = req.body
+    try {
+        const riwayat = new riwayatModel({
+            mahasiswa: req.user.id,
+            tanggal,
+            penyakit,
+            klinik,
+            pemeriksa
+        })
+        await riwayat.save()
+        req.flash('success', 'Izin sakit berhasil dikirim')
+        res.redirect('/mahasiswa/izin-sakit')
+    } catch (error) {
+        console.log(error)
+        res.redirect('/mahasiswa/izin-sakit')
+    }
+})
+
 router.get('/riwayat', async (req, res) => {
     try{
         const riwayat_pemeriksaan = await riwayatModel.find({mahasiswa: req.user.id}).sort({tanggal: -1})
